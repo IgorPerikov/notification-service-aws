dd641b4771af677090273971c72be47503d6cb6e
IgorPerikov <perikov.igor@gmail.com>
2020-02-07 13:40:30 +0300

    enable swagger

--- src/main/java/com/github/igorperikov/notification/NotificationServiceApplication.java
+++ src/main/java/com/github/igorperikov/notification/NotificationServiceApplication.java
@@ -5,10 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @Configuration
+    @EnableSwagger2
+    public static class AppConfiguration {
+
     }
 }
